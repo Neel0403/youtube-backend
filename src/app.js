668bea9d7c0effc764bbfb5aea4cdb258e708497
Neel0403 <neelo2003@gmail.com>
2024-07-02 16:48:57 +0530

    Add healthcheck feature

--- src/app.js
+++ src/app.js
@@ -15,11 +15,15 @@ import userRouter from "./routes/user.routes.js"; // userRouter can be named any
 import tweetRouter from "./routes/tweet.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import healthcheckRouter from "./routes/heathcheck.routes.js"
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
